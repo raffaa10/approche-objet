9aed610ef434bb915c4a61b255295d01a99ee9c3
Rafayel AZIZYAN <rafayel.azizyan@gmail.com>
2020-06-26 12:24:34 +0200

    First commit

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,27 @@
+package utils;
+
+import entites.AdressePostale;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+
+
+		String chaine = "12";
+		
+		System.out.println(chaine);
+		
+		int conv = Integer.valueOf(chaine);
+		
+		System.out.println(conv);
+		
+		
+		int a = 15;
+		int b = 25;
+		
+		int maxValue = Integer.max(a, b);
+			System.out.println(maxValue);
+		
+	}
+
+}
